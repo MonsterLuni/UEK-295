ecd9ad5aa4c73002ebfe306c8f6a9a0ebd6d83ae
MonsterLuni <monsterluni@gmail.com>
2023-05-04 16:59:14 +0200

    fix Database: Visual bug with ' in text & added check if entry exists

--- src/Controller/StoryController.php
+++ src/Controller/StoryController.php
@@ -78,15 +78,17 @@ class StoryController extends AbstractFOSRestController
         $dto = $this->serializer->deserialize($request->getContent(), CreateUpdateStory::class, "json");
         $entitystory = $this->repository->find($id);
 
+        if(!$entitystory) {
+            return $this->json("Story with ID " . $id . " does not exist! ", status: 403);
+        }
+
         $entitystory->setTitle($dto->title);
         $entitystory->setstorie($dto->storie);
         $entitystory->setLikes($dto->likes);
         $entitystory->setDislikes($dto->dislikes);
         $entitystory->setAuthor($dto->author);
 
-        if(!$entitystory) {
-            return $this->json("Story with ID " . $id . " doesn't exist!", status: 403);
-        }
+
         $this->repository->save($entitystory, true);
         return $this->json("Story with ID " . $id . " Succesfully Changed");
     }
