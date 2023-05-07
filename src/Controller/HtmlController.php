ab5fd9acf0a1872c7a39a964bd60761e5234ed83
MonsterLuni <monsterluni.specific@gmail.com>
2023-05-07 11:34:26 +0200

    feat Twig: add route for Twig ../api/html where storys get displayed

---
+++ src/Controller/HtmlController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+#[Route("/api", name: "api_")]
+class HtmlController extends AbstractController
+{
+    #[Rest\Get('/html', name: 'twig_html')]
+    public function html(StoryRepository $repository) : Response
+    {
+        $name = "Luca Moser";
+
+
+        $kommentar = $repository->findAll();
+
+        return $this->render('html/index.html.twig', [
+            'storys' => $kommentar,
+            'user_name' => $name,
+        ]);
+    }
+}
