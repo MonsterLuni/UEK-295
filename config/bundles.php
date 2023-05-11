014117e1c20364866e800d26342e48dbc25e1e8b
MonsterLuni <monsterluni@gmail.com>
2023-05-11 11:10:58 +0200

    feat Logging: Add Logging File for Viewing Info Logs

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
