ef2501ca8b668a1ca3d9a8d18ee2275093f594ab
MonsterLuni <monsterluni.specific@gmail.com>
2023-05-11 20:38:11 +0200

    feat Twig: Add Filter to UI with all Functions that are in Method filterAll

--- src/Controller/HtmlController.php
+++ src/Controller/HtmlController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\DTO\FilterStory;
 use App\Repository\StoryRepository;
+use JMS\Serializer\SerializerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -12,11 +16,28 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 class HtmlController extends AbstractController
 {
     #[Rest\Get('/home', name: 'twig_home')]
-    public function home(StoryRepository $repository) : Response
+    public function home(StoryRepository $repository, Request $request, SerializerInterface $serializer, LoggerInterface $logger) : Response
     {
         $name = "Luca Moser";
+        $filter = new FilterStory();
+        try {
+             $filter->author = $request->get("author");
+             $filter->likes = intval($request->get("likes"));
+             $filter->dislikes = intval($request->get("dislikes"));
+             if(!($request->get("sortby") == null)){
+                 $filter->orderby = $request->get("sortby");
+                 if($request->get("sortdirection") == "on"){
+                     $filter->orderdirection = "DESC";
+                 }
 
-        $kommentar = $repository->findAll();
+             }
+
+
+        }
+        catch(\Exception $ex){
+            $dtoFilter = new FilterStory();
+        }
+        $kommentar = $repository->filterAll($filter);
 
         return $this->render('html/index.html.twig', [
             'storys' => $kommentar,
