e41ef24754e2bb680465303fe17e1b28b252343c
MonsterLuni <monsterluni@gmail.com>
2023-05-12 13:00:22 +0200

    feat Authentification: Creat User in Database. Via route /api/user/register User can Register

---
+++ src/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\CreateUser;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\Post;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractFOSRestController
+{
+    public function __construct(private SerializerInterface $serializer,
+                                private UserRepository $repository){
+
+    }
+
+    #[Post('/api/user/register', name: 'api_user_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse{
+        $dto = $this->serializer->deserialize($request->getContent(), CreateUser::class, 'json');
+
+        $user = new User();
+        $user->setUsername($dto->username);
+        $hashedPassword = $passwordHasher->hashPassword($user,$dto->password);
+        $user->setPassword($hashedPassword);
+        if($dto->is_admin){
+            $user->setRoles(["ROLE_ADMIN", "ROLE_USER"]);
+        }
+
+        $this->repository->save($user,true);
+
+        return $this->json("User erstellt.");
+    }
+}
---
+++ src/DTO/CreateUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DTO;
+
+class CreateUser
+{
+public ?string $username = null;
+
+public ?string $password = null;
+public ?bool $is_admin = false;
+}
