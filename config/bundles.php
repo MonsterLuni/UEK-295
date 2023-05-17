535972fde3a2b82665016015121d1a0b98063e20
MonsterLuni <monsterluni@gmail.com>
2023-05-17 16:44:26 +0200

    feat Twig: Add Register Page + Changed Layout

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/HtmlController.php
+++ src/Controller/HtmlController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\StoryRepository;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use GuzzleHttp\Client;
 use JMS\Serializer\SerializerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/ArchiveOfMyself', name: 'ArchiveOfMyself_')]
 class HtmlController extends AbstractController
@@ -53,8 +55,8 @@ class HtmlController extends AbstractController
         }
     }
 
-    #[Rest\Get('/login', name: 'twig_html')]
-    public function login(Request $request, UserRepository $repository, UserPasswordHasherInterface $passwordHasher): Response
+    #[Rest\Get('/register', name: 'twig_register')]
+    public function register(Request $request, UserRepository $repository, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         if($request->get('username') != null){
@@ -68,9 +70,35 @@ class HtmlController extends AbstractController
             return $this->redirect('http://127.0.0.1:8000/ArchiveOfMyself/register');
         }
         else{
-            return $this->render('html/login.html.twig', [
+            return $this->render('html/register.html.twig', []);
+        }
+    }
 
-            ]);
+    #[Rest\Get('/login', name: 'twig_login')]
+    public function login(Request $request): Response
+    {
+        if($request->get('username_r') != null){
+            $user = new User();
+            $user->setUsername($request->get('username_l'));
+            $user->setPassword($request->get('password_l'));
+
+            $client = new Client();
+            $response = $client->post("http://localhost:8000/api/login_check",
+                [
+                    "json" => json_encode(
+                        [
+                            "username" => $user->getUsername(),
+                            "password" => $user->getPassword()
+                        ]),
+                    'timeout' => 10
+                ]
+            );
+
+            var_dump($response->getBody()); die;
+            return $this->redirect('http://127.0.0.1:8000/ArchiveOfMyself/home');
+        }
+        else{
+            return $this->render('html/login.html.twig');
         }
     }
 }
